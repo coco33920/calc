8169e8c6544d053cea338d39f07ac41d7c28755d
Charlotte Thomas <contact@nwa2coco.fr>
2023-11-20 12:21:23 +0100

    bump to 2.9.3 fix bug

--- src/configuration/loader.rs
+++ src/configuration/loader.rs
@@ -131,7 +131,7 @@ pub fn load_color(string: String) -> Color {
 
 pub fn replace_variable(str: String) -> String {
     str.replace("%author%", "Charlotte Thomas")
-        .replace("%version%", "v2.9.2")
+        .replace("%version%", "v2.9.3")
         .to_string()
 }
 

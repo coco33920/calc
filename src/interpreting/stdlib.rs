02c194dc21316a77ae1af146fea7226faf976b40
Charlotte Thomas <contact@nwa2coco.fr>
2023-11-23 11:52:57 +0100

    vectorisation cosh

--- src/interpreting/stdlib.rs
+++ src/interpreting/stdlib.rs
@@ -343,6 +343,37 @@ pub fn cosh(p: &Vec<Parameters>, ram: &Option<&mut HashMap<String, Parameters>>)
             let fs: f64 = if degrees { (*f) * (PI / 180.0) } else { *f };
             Parameters::Float(fs.cosh())
         }
+
+        Parameters::InterpreterVector(vec) => {
+            let mut res = Vec::new();
+            vec.clone().into_iter().for_each(|x| match x {
+                Parameters::Int(i) => res.push(Parameters::Float(if degrees {
+                    ((i as f64) * PI / 180.0).cosh()
+                } else {
+                    (i as f64).cosh()
+                })),
+                Parameters::Float(f) => res.push(Parameters::Float(if degrees {
+                    (f * PI / 180.0).cosh()
+                } else {
+                    f.cosh()
+                })),
+                Parameters::Identifier(s) => match ram {
+                    None => (),
+                    Some(ref t) => match t.get(s.as_str()) {
+                        None => (),
+                        Some(s) => {
+                            if degrees {
+                                res.push(cosh(&vec![s.clone(), Parameters::Bool(false)], ram))
+                            } else {
+                                res.push(cosh(&vec![s.clone()], ram))
+                            }
+                        }
+                    },
+                },
+                _ => (),
+            });
+            Parameters::InterpreterVector(Box::from(res))
+        }
         Parameters::Identifier(s) => match ram {
             None => Parameters::Identifier("This variable is not initialized yet".to_string()),
             Some(ref t) => match t.get(s.as_str()) {

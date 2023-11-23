548b57fb31040faf31e04362671e7229ed211061
Charlotte Thomas <contact@nwa2coco.fr>
2023-11-23 11:58:23 +0100

    vectorisation of atan

--- src/interpreting/stdlib.rs
+++ src/interpreting/stdlib.rs
@@ -736,6 +736,37 @@ pub fn atan(p: &Vec<Parameters>, ram: &Option<&mut HashMap<String, Parameters>>)
         } else {
             f.atan()
         }),
+
+        Parameters::InterpreterVector(vec) => {
+            let mut res = Vec::new();
+            vec.clone().into_iter().for_each(|x| match x {
+                Parameters::Int(i) => res.push(Parameters::Float(if degrees {
+                    ((i as f64) * PI / 180.0).atan()
+                } else {
+                    (i as f64).atan()
+                })),
+                Parameters::Float(f) => res.push(Parameters::Float(if degrees {
+                    (f * PI / 180.0).atan()
+                } else {
+                    f.atan()
+                })),
+                Parameters::Identifier(s) => match ram {
+                    None => (),
+                    Some(ref t) => match t.get(s.as_str()) {
+                        None => (),
+                        Some(s) => {
+                            if degrees {
+                                res.push(atan(&vec![s.clone(), Parameters::Bool(false)], ram))
+                            } else {
+                                res.push(atan(&vec![s.clone()], ram))
+                            }
+                        }
+                    },
+                },
+                _ => (),
+            });
+            Parameters::InterpreterVector(Box::from(res))
+        }
         Parameters::Identifier(s) => match ram {
             None => Parameters::Identifier("This variable is not initialized yet".to_string()),
             Some(ref t) => match t.get(s.as_str()) {

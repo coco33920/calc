b98e705cf6e96da5405687edd04f7bc1507b018e
Charlotte Thomas <contact@nwa2coco.fr>
2023-11-23 11:48:38 +0100

    sin vectorisation

--- src/interpreting/stdlib.rs
+++ src/interpreting/stdlib.rs
@@ -188,6 +188,36 @@ pub fn sin(p: &Vec<Parameters>, ram: &Option<&mut HashMap<String, Parameters>>) 
             let fs: f64 = if degrees { (*f) * (PI / 180.0) } else { *f };
             Parameters::Float(fs.sin())
         }
+        Parameters::InterpreterVector(vec) => {
+            let mut res = Vec::new();
+            vec.clone().into_iter().for_each(|x| match x {
+                Parameters::Int(i) => res.push(Parameters::Float(if degrees {
+                    ((i as f64) * PI / 180.0).cos()
+                } else {
+                    (i as f64).cos()
+                })),
+                Parameters::Float(f) => res.push(Parameters::Float(if degrees {
+                    (f * PI / 180.0).cos()
+                } else {
+                    f.cos()
+                })),
+                Parameters::Identifier(s) => match ram {
+                    None => (),
+                    Some(ref t) => match t.get(s.as_str()) {
+                        None => (),
+                        Some(s) => {
+                            if degrees {
+                                res.push(cos(&vec![s.clone(), Parameters::Bool(false)], ram))
+                            } else {
+                                res.push(cos(&vec![s.clone()], ram))
+                            }
+                        }
+                    },
+                },
+                _ => (),
+            });
+            Parameters::InterpreterVector(Box::from(res))
+        }
         Parameters::Identifier(s) => match ram {
             None => Parameters::Identifier("This variable is not initialized yet".to_string()),
             Some(ref t) => match t.get(s.as_str()) {
